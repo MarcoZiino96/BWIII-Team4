999fbcd19e7daa6ed815e1995c32f16ee7bdaba4
fuza22 <barone.emanuele8@gmail.com>
2023-12-11 13:00:49 +0100

    navbar-footer

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
