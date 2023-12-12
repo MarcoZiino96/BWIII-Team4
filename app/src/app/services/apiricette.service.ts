4b5dc914fa61aaecc29741a6c4e6593f13f06ebe
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-12-12 11:52:37 +0100

    service

--- app/src/app/services/apiricette.service.ts
+++ app/src/app/services/apiricette.service.ts
@@ -18,4 +18,20 @@ export class APIricetteService {
     return this.http.get<iRicetta[]>(`${environment.apiUrl}?categoria=${categoria}`)
   }
 
+  create(ricetta:Partial<iRicetta>) : Observable<iRicetta>{
+    return this.http.post<iRicetta>(environment.apiUrl,ricetta)
+  }
+
+  update(ricetta:iRicetta) : Observable<iRicetta>{
+    return this.http.put<iRicetta>(`${environment.apiUrl}/${ricetta.id}`,ricetta)
+  }
+
+  delete(id:number) : Observable <iRicetta> | undefined{
+    return this.http.delete<iRicetta>(`${environment.apiUrl}/${id}`)
+  }
+
+  getById(id:number) : Observable<iRicetta>{
+    return this.http.get<iRicetta>(`${environment.apiUrl}/${id}`)
+  }
+
 }
