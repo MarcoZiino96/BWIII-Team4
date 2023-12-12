03453b5c37242c37e1b501572dbae5898ede9489
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-12-12 11:21:53 +0100

    commit per Giulio <3

--- app/src/app/services/apiricette.service.ts
+++ app/src/app/services/apiricette.service.ts
@@ -1,9 +1,21 @@
+import { iRicetta } from './../Models/iricetta';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIricetteService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll(): Observable<iRicetta[]>{
+    return this.http.get<iRicetta[]>(environment.apiUrl)
+  }
+  getByCat(categoria:string): Observable<iRicetta[]> | undefined{
+    return this.http.get<iRicetta[]>(`${environment.apiUrl}?categoria=${categoria}`)
+  }
+
 }
