4136d2dbc069f96a8614f761a65b43e43b090376
Mauro-Cassoni <mauro.cassoni.94@gmail.com>
2023-12-13 18:06:54 +0100

    push pre pull

--- app/src/app/components/navbar/navbar.component.ts
+++ app/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { APIricetteService } from '../../services/apiricette.service';
+import { iRicetta } from '../../Models/iricetta';
 
 @Component({
   selector: '.app-navbar',
@@ -11,5 +12,17 @@ export class NavbarComponent {
   open:boolean = false;
 
   constructor(private apiSvc:APIricetteService){}
+
+  ricetteArr:iRicetta[] = [];
+
+
+
+
+  ngOnInit(){
+    this.apiSvc.getAll().subscribe(data =>{
+      this.ricetteArr = data
+      console.log(this.ricetteArr)
+    })
+  }
 
 }
