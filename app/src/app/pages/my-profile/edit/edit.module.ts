708f87173ef04066c6b522a2a479a770393a6cfb
giuliomarinelli <giulio.marinelli@icloud.com>
2023-12-15 12:52:19 +0100

    create and update ok

--- app/src/app/pages/my-profile/edit/edit.module.ts
+++ app/src/app/pages/my-profile/edit/edit.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { EditRoutingModule } from './edit-routing.module';
 import { EditComponent } from './edit.component';
-import { RecipeFormComponent } from '../../../components/recipe-form/recipe-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 import { NzSelectModule } from 'ng-zorro-antd/select';
@@ -12,8 +10,7 @@ import { MyProfileModule } from '../my-profile.module';
 
 @NgModule({
   declarations: [
-    EditComponent,
-    RecipeFormComponent
+    EditComponent
   ],
   imports: [
     CommonModule,
